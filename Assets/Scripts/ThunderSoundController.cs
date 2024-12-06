59fd6f7f36f28124e80ceea82c50ddbd90eca7de
Arman <144450534+JoeMac12@users.noreply.github.com>
2024-12-06 05:37:59 -0400

    Made multiple audio sources for now

--- Assets/Scripts/ThunderSoundController.cs
+++ Assets/Scripts/ThunderSoundController.cs
@@ -2,37 +2,57 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// This is some cursed code. i'll fix it another time
 public class ThunderSoundController : MonoBehaviour
 {
-	public AudioSource audioSource;
 	public AudioClip[] thunderSounds;
 	public float volume = 1f;
+	public int maxSounds = 3;
+
+	private Queue<AudioSource> availableAudioSources;
+	private List<AudioSource> activeAudioSources;
 
 	private void Start()
 	{
-		if (audioSource == null)
-		{
-			audioSource = GetComponent<AudioSource>();
-		}
+		InitializeAudioSources();
+	}
 
-		if (audioSource == null)
-		{
-			enabled = false;
-			return;
-		}
+	private void InitializeAudioSources()
+	{
+		availableAudioSources = new Queue<AudioSource>();
+		activeAudioSources = new List<AudioSource>();
 
-		audioSource.volume = volume;
+		for (int i = 0; i < maxSounds; i++)
+		{
+			AudioSource source = gameObject.AddComponent<AudioSource>();
+			source.volume = volume;
+			source.playOnAwake = false;
+			availableAudioSources.Enqueue(source);
+		}
+	}
+
+	private void Update()
+	{
+		for (int i = activeAudioSources.Count - 1; i >= 0; i--)
+		{
+			if (!activeAudioSources[i].isPlaying)
+			{
+				availableAudioSources.Enqueue(activeAudioSources[i]);
+				activeAudioSources.RemoveAt(i);
+			}
+		}
 	}
 
 	public void PlayThunderSound()
 	{
-		if (thunderSounds == null || thunderSounds.Length == 0)
+		if (thunderSounds == null || thunderSounds.Length == 0 || availableAudioSources.Count == 0)
 		{
 			return;
 		}
 
-		int randomIndex = Random.Range(0, thunderSounds.Length);
-		audioSource.clip = thunderSounds[randomIndex];
+		AudioSource audioSource = availableAudioSources.Dequeue();
+		audioSource.clip = thunderSounds[Random.Range(0, thunderSounds.Length)];
 		audioSource.Play();
+		activeAudioSources.Add(audioSource);
 	}
 }
